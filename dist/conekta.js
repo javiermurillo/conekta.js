e72f3eab79cb09836d05206be18e4f0153218963
Javier Murillo <jhmmurillo@gmail.com>
2015-07-06 17:05:07 -0500

    remove use of jsonp2 from request, is mandatory make strong test with this file, because touch every tokenization call

--- dist/conekta.js
+++ dist/conekta.js
@@ -475,17 +475,23 @@
             });
           };
           if (document.location.protocol === 'file:') {
-            params.url = (params.jsonp_url || params.url) + '/create.js';
-            params.data['_Version'] = "0.3.0";
-            params.data['_RaiseHtmlError'] = false;
-            params.data['auth_token'] = Conekta.getPublishableKey();
-            params.data['conekta_client_user_agent'] = '{"agent":"Conekta JavascriptBindings/0.3.0"}';
             return ajax({
-              url: base_url + params.url,
-              dataType: 'jsonp',
-              data: params.data,
-              success: success_callback,
-              error: error_callback
+                url: base_url + params.url,
+                type: 'POST',
+                dataType: 'json',
+                data: JSON.stringify(params.data),
+                contentType: 'application/json',
+                headers: {
+                    'RaiseHtmlError': false,
+                    'Accept': 'application/vnd.conekta-v0.3.0+json',
+                    'Accept-Language': Conekta.getLanguage(),
+                    'Conekta-Client-User-Agent': '{"agent":"Conekta
+                    JavascriptBindings/0.3.0"}',
+                    'Authorization': 'Basic ' +
+                    Base64.encode(Conekta.getPublishableKey() + ':')
+                },
+                success: success_callback,
+                error: error_callback
             });
           } else {
             if (typeof (new XMLHttpRequest()).withCredentials !== 'undefined') {
